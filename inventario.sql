3379fe8f7f36096a9bc39f8cadf6dd2af8d99640
ldls1998 <86206834+ldls1998@users.noreply.github.com>
2021-08-23 00:30:17 -0500

    Update inventario.sql

--- inventario.sql
+++ inventario.sql
@@ -1,7 +1,7 @@
 -- phpMyAdmin SQL Dump
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
---
+
 -- Servidor: 127.0.0.1
 -- Tiempo de generación: 05-07-2021 a las 19:27:50
 -- Versión del servidor: 10.4.19-MariaDB
